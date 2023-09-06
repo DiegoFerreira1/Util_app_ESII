f297b5da0f7fe1f815ca48c37bbffe96677cee58
Diego Ferreira <diego19912011@live.com>
2023-09-06 16:29:26 -0300

    Tela de Cadastro finalizada

--- app/src/main/java/com/example/utilapp/MainLogin.java
+++ app/src/main/java/com/example/utilapp/MainLogin.java
@@ -2,13 +2,31 @@ package com.example.utilapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        IniciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainLogin.this, MainCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.id_text_cadastro);
     }
 }
